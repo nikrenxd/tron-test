f5cbf2e38d36b58f6348b400d34d493d44d70140
nikrenxd <nikrenc1101@gmail.com>
2025-06-09 18:11:50 +0300

    created repository and services

---
+++ src/services/wallets.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+from tronpy import AsyncTron
+
+from src.repositories.wallets import WalletsRepository
+
+
+@dataclass
+class WalletsServices:
+    repository: WalletsRepository
+
+    async def get_account_info(self, wallet_address: str) -> dict:
+        async with AsyncTron(network="shasta") as client:
+            account_balance = await client.get_account_balance(wallet_address)
+            resources = await client.get_account_resource(wallet_address)
+
+            bandwidth_limit = resources.get("freeNetLimit")
+            bandwidth_left = bandwidth_limit - resources.get("freeNetUsed", 0)
+
+            energy_limit = resources.get("EnergyLimit")
+            energy_left = energy_limit - resources.get("EnergyUsed", 0)
+
+            account_data = {
+                "wallet_address": wallet_address,
+                "balance": account_balance,
+                "bandwidth_limit": bandwidth_limit,
+                "bandwidth_left": bandwidth_left,
+                "energy_limit": energy_limit,
+                "energy_left": energy_left,
+            }
+
+            return account_data
+
+    async def get_last_accounts_info(self):
+        pass
+
+    async def save_account_info(self):
+        pass
